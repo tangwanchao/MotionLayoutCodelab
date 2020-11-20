f6c4ef1d4752529dc8341a57485b6425d68091fe
tangwanchao <18284228392@163.com>
2020-11-20 15:40:54 +0800

    代码控制运动完成

--- app/src/main/java/me/twc/motionlayoutcodelab/MainActivity.kt
+++ app/src/main/java/me/twc/motionlayoutcodelab/MainActivity.kt
@@ -2,10 +2,25 @@ package me.twc.motionlayoutcodelab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        coordinateMotion()
+    }
+
+    private fun coordinateMotion() {
+        val appBarLayout: AppBarLayout = findViewById(R.id.appbar_layout)
+        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+
+        val listener = AppBarLayout.OnOffsetChangedListener { unused, verticalOffset ->
+            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+            motionLayout.progress = seekPosition
+        }
+
+        appBarLayout.addOnOffsetChangedListener(listener)
     }
 }
